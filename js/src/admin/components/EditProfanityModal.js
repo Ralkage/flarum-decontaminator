709b6328ea7827509fb1c7374845d2c84a94aa1d
Ian Morland <ian@morland.me>
2020-04-15 09:35:54 +0100

    Update create/edit modal

--- js/src/admin/components/EditProfanityModal.js
+++ js/src/admin/components/EditProfanityModal.js
@@ -1,5 +1,6 @@
 import Modal from 'flarum/components/Modal';
 import Button from 'flarum/components/Button';
+import Switch from 'flarum/components/Switch';
 
 /**
  * The `EditPageModal` component shows a modal dialog which allows the user
@@ -32,79 +33,83 @@ export default class EditPageModal extends Modal {
         <div className="Form">
 
           <div className="Form-group">
-            <label>{app.translator.trans('imorland-post-decontaminator.admin.edit_profanity.name_label')}</label>
-            <input
-              className="FormControl"
-              placeholder=''
-              value={this.name()}
-              oninput={e => {
-                this.name(e.target.value);
-              }}
-            />
+            <label>{app.translator.trans('imorland-post-decontaminator.admin.edit_profanity.name_label')}
+              <input
+                className="FormControl"
+                placeholder=''
+                value={this.name()}
+                oninput={e => {
+                  this.name(e.target.value);
+                }}
+              />
+            </label>
           </div>
           <div className="Form-group">
-            <label>{app.translator.trans('imorland-post-decontaminator.admin.edit_profanity.regex_label')}</label>
-            <input
-              className="FormControl"
-              placeholder=''
-              value={this.regex()}
-              oninput={e => {
-                this.regex(e.target.value);
-              }}
-            />
+            <label>{app.translator.trans('imorland-post-decontaminator.admin.edit_profanity.regex_label')}
+              <input
+                className="FormControl"
+                placeholder=''
+                value={this.regex()}
+                oninput={e => {
+                  this.regex(e.target.value);
+                }}
+              />
+            </label>
           </div>
 
           <div className="Form-group">
-            <label>{app.translator.trans('imorland-post-decontaminator.admin.edit_profanity.replacement_label')}</label>
-            <input
-              className="FormControl"
-              placeholder=''
-              value={this.replacement()}
-              oninput={e => {
-                this.replacement(e.target.value);
-              }}
-            />
+            <label>{app.translator.trans('imorland-post-decontaminator.admin.edit_profanity.replacement_label')}
+              <input
+                className="FormControl"
+                placeholder=''
+                value={this.replacement()}
+                oninput={e => {
+                  this.replacement(e.target.value);
+                }}
+              />
+            </label>
           </div>
 
           <div className="Form-group">
             <label>{app.translator.trans('imorland-post-decontaminator.admin.edit_profanity.flag_label')}</label>
             <input
-              className="FormControl"
-              value={this.flag()}
-              oninput={e => {
-                this.flag(e.target.value);
+              type="checkbox"
+              checked={this.flag()}
+              onclick={e => {
+                this.flag(e.target.checked);
               }}
             />
           </div>
 
           <div className="Form-group">
-            <label>{app.translator.trans('imorland-post-decontaminator.admin.edit_profanity.applywhen_label')}</label>
-            <select className="FormControl"
+            <label>{app.translator.trans('imorland-post-decontaminator.admin.edit_profanity.applywhen_label')}
+              <select className="FormControl"
 
-                    oninput={m.withAttr('value', this.event)}
-                    value={this.event()}
-            >
-              <option value="save">Saving a Post</option>
-              <option value="load">Loading a Post (this can have an adverse performance impact)
+                oninput={m.withAttr('value', this.event)}
+                value={this.event()}
+              >
+                <option value="save">{app.translator.trans('imorland-post-decontaminator.admin.edit_profanity.action.save')}</option>
+                <option value="load">{app.translator.trans('imorland-post-decontaminator.admin.edit_profanity.action.load')}
               </option>
-            </select>
+              </select>
+            </label>
           </div>
 
           <div className="Form-group">
             {Button.component({
-                                type: 'submit',
-                                className: 'Button Button--primary EditProfanityModal-save',
-                                loading: this.loading,
-                                children: app.translator.trans('imorland-post-decontaminator.admin.edit_profanity.submit_button'),
-                              })}
+              type: 'submit',
+              className: 'Button Button--primary EditProfanityModal-save',
+              loading: this.loading,
+              children: app.translator.trans('imorland-post-decontaminator.admin.edit_profanity.submit_button'),
+            })}
             {this.profanity.exists ? (
               <button type="button" className="Button EditProfanityModal-delete"
-                      onclick={this.delete.bind(this)}>
-                      {app.translator.trans('imorland-post-decontaminator.admin.edit_profanity.delete_button')}
+                onclick={this.delete.bind(this)}>
+                {app.translator.trans('imorland-post-decontaminator.admin.edit_profanity.delete_button')}
               </button>
             ) : (
-               ''
-             )}
+                ''
+              )}
           </div>
         </div>
       </div>
@@ -125,7 +130,7 @@ export default class EditPageModal extends Modal {
         replacement: this.replacement(),
         type: 'profanity',
       },
-      {errorHandler: this.onerror.bind(this)}
+      { errorHandler: this.onerror.bind(this) }
     )
       .then(this.hide.bind(this))
       .catch(() => {
